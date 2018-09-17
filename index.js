d2dd132c273481b419890e5358b716484d6cdf8e
Benni Austin <benniaustindev@gmail.com>
2018-09-16 22:10:07 -0500

    show header onload when page is scrolled

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded',function(event){
+
+  if(window.scrollY!==0){
+    let header=document.querySelector('header');
+    header.style.top=window.scrollY+'px';
+    window.setTimeout(function(){
+      header.classList.add('adjusted');
+      window.setTimeout(function(){
+        header.style.top='0px';
+      },1);
+    },1);
+    header.addEventListener('transitionend',(event)=>{
+      header.classList.remove('adjusted');
+      header.style.top=null;
+    })
+  }
+
+
   let techButtons=Array.from(document.querySelectorAll('#technologies button'));
   techButtons.forEach(button=>{
     button.addEventListener('click',function(event){
