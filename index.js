526361c434e30025e2490e9f7da8c4b648814f66
Benni Austin <benniaustindev@gmail.com>
2018-09-16 21:37:34 -0500

    remove ability to select none, on technology items

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ document.addEventListener('DOMContentLoaded',function(event){
   let techButtons=Array.from(document.querySelectorAll('#technologies button'));
   techButtons.forEach(button=>{
     button.addEventListener('click',function(event){
-      if(this.classList.contains('active')){
-        this.classList.remove('active');
-      }
-      else {
-        techButtons.forEach(button=>button.classList[(button==this)?'add':'remove']('active'));
-      }
-    })
-  })
-})
+      techButtons.forEach(button=>{
+        button.classList[(button==this)?'add':'remove']('active');
+      });
+    });
+  });
+});
